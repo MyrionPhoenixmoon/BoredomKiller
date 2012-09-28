273ec0f13fff41652c144871f0119e0636ae0263
Patrick Manser <pm@cyon.ch>
2012-09-28 14:19:10 +0200

    Minor improvement: Changed some primary keys to foreign keys
    All the relation keys in the script were primary keys. This is both
    ugly and unnecessary. I changed those primary keys to foreign keys.

--- src/bk_db_wip.sql
+++ src/bk_db_wip.sql
@@ -6,23 +6,22 @@
 -- --------------------------------------------------
 -- ----------------- Change log ---------------------
 -- --------------------------------------------------
+-- 28.09.12 - V0.2 - Changed some primary keys to foreign keys
 -- 28.09.12 - V0.1 - Added the create database command
 -- 28.09.12 - V0.1 - Added relations (pm)
 -- 28.09.12 - V0.1 - Created all main tables (pm)
 
--- Create the database if it doesn't exist already
+-- Drop database and then create it
+drop database if exists boredomkiller;
 create database if not exists boredomkiller;
 
--- Games table
-create table if not exists tbl_games (
+
+-- Statistics table
+create table if not exists tbl_statistics (
     id int not null auto_increment,
-    name varchar(100) not null,
-    min_players int not null,
-    max_players int not null,
-    theme_loc varchar(200),
-    id_statistic int not null,
-    id_platform int not null,
-    primary key (id, id_statistic, id_platform)
+    times_played int,
+    total_playtime int,
+    primary key (id)
 );
 
 -- Platforms table
@@ -39,55 +38,11 @@ create table if not exists tbl_playstyles (
     primary key (id)
 );
 
--- Modes table
-create table if not exists tbl_modes (
-    id int not null auto_increment,
-    name varchar(50) not null,
-    id_game int not null,
-    primary key (id, id_game)
-);
-
--- Challenges table
-create table if not exists tbl_challenges (
-    id int not null auto_increment,
-    name varchar(50) not null,
-    description text not null,
-    id_difficulty int not null,
-    id_statistic int not null,
-    id_game_type int not null,
-    id_game int not null,
-    primary key (id, id_difficulty, id_statistic, id_game_type, id_game)
-);
-
 -- Game Types table
 create table if not exists tbl_game_types (
     id int not null auto_increment,
     name varchar(50) not null,
     primary key (id)
-);
-
--- Players table
-create table if not exists tbl_players (
-    id int not null auto_increment,
-    name varchar(20) not null,
-    id_statistic,
-    primary key (id, id_statistic)
-);
-
--- Statistics table
-create table if not exists tbl_statistics (
-    id int not null auto_increment,
-    times_played int,
-    total_playtime int
-    primary key (id)
-);
-
--- Maps table
-create table if not exists tbl_maps (
-    id int not null auto_increment,
-    name varchar(50) not null,
-    id_game int not null,
-    primary key (id, id_game)
 );
 
 -- Maluses table
@@ -97,6 +52,70 @@ create table if not exists tbl_maluses (
     description text not null,
     rating varchar(45) not null,
     primary key (id)
+);
+
+-- Players table
+create table if not exists tbl_players (
+    id int not null auto_increment,
+    name varchar(20) not null,
+    id_statistic int,
+    primary key (id),
+    foreign key (id_statistic) references tbl_statistics (id)
+);
+
+-- Games table
+create table if not exists tbl_games (
+    id int not null auto_increment,
+    name varchar(100) not null,
+    min_players int not null,
+    max_players int not null,
+    theme_loc varchar(200),
+    id_statistic int not null,
+    id_platform int not null,
+    primary key (id),
+    foreign key (id_statistic) references tbl_statistics (id),
+    foreign key (id_platform) references tbl_platforms (id)
+);
+
+-- Difficulties table
+create table if not exists tbl_difficulties (
+    id int not null auto_increment,
+    difficulty varchar(30) not null,
+    primary key (id)
+);
+
+-- Challenges table
+create table if not exists tbl_challenges (
+    id int not null auto_increment,
+    name varchar(50) not null,
+    description text not null,
+    id_difficulty int not null,
+    id_statistic int not null,
+    id_game_type int,
+    id_game int,
+    primary key (id),
+    foreign key (id_difficulty) references tbl_difficulties (id),
+    foreign key (id_statistic) references tbl_statistics (id),
+    foreign key (id_game_type) references tbl_game_types (id),
+    foreign key (id_game) references tbl_games (id)
+);
+
+-- Modes table
+create table if not exists tbl_modes (
+    id int not null auto_increment,
+    name varchar(50) not null,
+    id_game int not null,
+    primary key (id),
+    foreign key (id_game) references tbl_games (id)
+);
+
+-- Maps table
+create table if not exists tbl_maps (
+    id int not null auto_increment,
+    name varchar(50) not null,
+    id_game int not null,
+    primary key (id),
+    foreign key (id_game) references tbl_games (id)
 );
 
 -- Relation Map - Playstyle
