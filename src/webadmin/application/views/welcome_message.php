6cab9fa94a75b99a1bbf6a67648e0ffc19e9833b
root <root@mansi-lapper.(none)>
2012-10-21 14:36:24 +0200

    Added: WebAdmin filesystem (CodeIgniter)
    .gitignore will be updated soon

---
+++ src/webadmin/application/views/welcome_message.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/css/main.css">
+        <title>BoredomKiller WebAdmin</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
