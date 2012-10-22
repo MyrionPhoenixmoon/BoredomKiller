71f1517f6534cbaf539fae0e8c4c0acb40e332e2
root <root@mansi-lapper.(none)>
2012-10-22 02:12:28 +0200

    added: first design
    criticism is very much appreciated!

--- src/webadmin/application/views/welcome_message.php
+++ src/webadmin/application/views/welcome_message.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Patrick Manser | Team pajama">
+        <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/css/main.css">
         <title>BoredomKiller WebAdmin</title>
     </head>
     <body>
-        
+        <div id="wrapper">
+            <header id="page-header">
+                <h1>BoredomKiller <span>::WebAdmin</span></h1>
+            </header>
+            
+            <nav id="page-nav">
+                <a href="">dashboard</a>
+                <a href="">games</a>
+                <a href="">challenges</a>
+                <a href="">maluses</a>
+                <a href="">players</a>
+            </nav>
+            
+            <section id="page-content">
+                <h1>Heading 1</h1>
+                <h2>Heading 2</h2>
+                <h3>Heading 3</h3>
+                <p>Paragraph. <a href="">Link</a></p>
+            </section>
+            
+            <footer id="page-footer">
+                &copy; 2012 by Patrick Manser (Team pajama). All rights reserved.
+            </footer>
+        </div>
     </body>
 </html>
